2d6f122b980d02bf499d3b18c2797adba475e4da
Taras.Kravets <taraskravets95@gmail.com>
2023-05-10 14:01:31 +0300

    init: init storybook-project

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+export interface IButton {
+	label?: string;
+	backgroundColor?: string;
+	size?: 'sm' | 'md' | 'lg';
+	handleClick?: () => void;
+}
+
+function Button({
+	label,
+	backgroundColor = 'red',
+	size = 'md',
+	handleClick,
+}: IButton) {
+	let scale = 1;
+	if (size === 'sm') scale = 0.75;
+	if (size === 'lg') scale = 1.5;
+	const style = {
+		backgroundColor,
+		padding: `${scale * 0.5}rem ${scale * 1}rem`,
+		border: 'none',
+	};
+	return (
+		<button onClick={handleClick} style={style}>
+			{label}
+		</button>
+	);
+}
+
+export default Button;
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,23 @@
+import Button from '../components/Button';
+
+// eslint-disable-next-line storybook/story-exports
+export default {
+	title: 'Button',
+	component: Button,
+};
+
+const Template = (args: any) => <Button {...args} />;
+
+export const Red = () => Template.bind({});
+Red.args = {
+	label: 'Press me',
+	backgroundColor: 'red',
+	size: 'md',
+};
+
+export const Green = Template.bind({});
+Green.args = {
+	label: 'Press Me',
+	backgroundColor: 'green',
+	size: 'md',
+};
